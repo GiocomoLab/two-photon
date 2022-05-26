7800363140ca388b623f96ef50d3f7af40b91b7e
alex-attinger <a.attinger@gmail.com>
2022-05-26 11:59:34 -0700

    example batch files

---
+++ two-photon/cmd_folder/multiple_2022-05-25_17_20_57.bat
@@ -0,0 +1,5 @@
+timeout 1
+python C:\code\two-photon\two-photon\process.py --input_dir E:\AA --output_dir F:\AA\output --channel 2 --recording 20220525_C80M1:vr_stim-000 --preprocess --zip_data --backup_data --backup_dir=Y:\giocomo\export\data\Projects\multiSLM\2p_Data
+python C:\code\two-photon\two-photon\process.py --input_dir E:\AA --output_dir F:\AA\output --channel 2 --recording 20220525_C80M1:vr_dark-001 --preprocess --zip_data --backup_data --backup_dir=Y:\giocomo\export\data\Projects\multiSLM\2p_Data
+python C:\code\two-photon\two-photon\process.py --input_dir E:\AA --output_dir F:\AA\output --channel 2 --recording 20220525_C80M1:vr_dark-001 --run_suite2p --prev_recording 20220525_C80M1:vr_stim-000
+python C:\code\two-photon\two-photon\process.py --input_dir E:\AA --output_dir F:\AA\output --channel 2 --recording 20220525_C80M2:vr_stim-002 --preprocess --zip_data --backup_data --backup_dir=Y:\giocomo\export\data\Projects\multiSLM\2p_Data
---
+++ two-photon/cmd_folder/multiple_2022-05-25_17_23_56.bat
@@ -0,0 +1,3 @@
+python C:\code\two-photon\two-photon\process.py --input_dir E:\AA --output_dir F:\AA\output --channel 2 --recording 20220525_C80M2:vr_stim-002 --preprocess 
+python C:\code\two-photon\two-photon\process.py --input_dir E:\AA --output_dir F:\AA\output --channel 2 --recording 20220525_C80M2:vr_dark-003 --preprocess
+
